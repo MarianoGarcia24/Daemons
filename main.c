30636f672ce817018d8e323de67eb63fdd527ccd
Ramiro Pruis <82285826+RamiroPruis@users.noreply.github.com>
2023-05-19 15:56:29 -0300

    deamon skeleton

--- main.c
+++ main.c
@@ -1,35 +1,65 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <syslog.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <dirent.h>
 
-int main()
-{
-    pid_t pid, sid;
-    
-    // Crear un proceso hijo
-    pid = fork();
+void signalHandler(int sig) {
+    // Manejar señales aquí
+}
+
+int main() {
+    // Crear un nuevo proceso
+    pid_t pid = fork();
+
+    // Salir si el fork falla
     if (pid < 0) {
-        exit(EXIT_FAILURE);
-    }
-    
-    // Si el proceso padre puede crear el proceso hijo, entonces termina
-    if (pid > 0) {
-        exit(EXIT_SUCCESS);
+        printf("Error al crear el proceso hijo.\n");
+        exit(1);
     }
 
-    // Inicializar el registro del sistema
-    openlog("simple_daemon", LOG_PID, LOG_DAEMON);
+    // Salir del proceso padre
+    if (pid > 0) {
+        exit(0);
+    }
+
+    // Establecer el modo de archivo y directorio
+    umask(0);
+
+    // Crear una nueva sesión de grupo
+    pid_t sid = setsid();
+    if (sid < 0) {
+        printf("Error al crear la nueva sesión de grupo.\n");
+        exit(1);
+    }
+
+    // Cerrar los descriptores de archivo estándar
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    // Establecer el manejador de señales
+    signal(SIGTERM, signalHandler);  // Por ejemplo, manejar SIGTERM
 
     // Bucle principal del daemon
     while (1) {
-        // Realizar las tareas del daemon aquí
-        syslog(LOG_NOTICE, "El daemon está ejecutándose.");
-        sleep(10); // Esperar 10 segundos antes de la siguiente iteración
+        
+
+        sleep(1);  // Ejemplo: Esperar 1 segundo antes de repetir el bucle
     }
 
-    // Cerrar el registro del sistema
-    closelog();
+    return 0;
+}
 
-    exit(EXIT_SUCCESS);
+void makeBackup(){
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir("./test");
+
+
+
+    return 0;
 }
